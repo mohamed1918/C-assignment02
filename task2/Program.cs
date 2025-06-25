a56b1c2b44521f9ce705572f92c7e044b897f116
mohamed1918 <memoohossam2004@gmail.com>
2025-06-25 15:06:28 +0300

    Q6

--- task2/Program.cs
+++ task2/Program.cs
@@ -31,10 +31,17 @@ namespace task2
             #endregion
 
             #region Q5
-            int a = 10;
-            int b = a;
-            b = 20;
-            Console.WriteLine($"a = {a}, b = {b}");
+            //int a = 10;
+            //int b = a;
+            //b = 20;
+            //Console.WriteLine($"a = {a}, b = {b}");
+            #endregion
+
+            #region Q6
+            int[] arr1 = { 1, 2, 3 };
+            int[] arr2 = arr1;
+            arr2[0] = 99;
+            Console.WriteLine($"arr1[0] = {arr1[0]}");
             #endregion
         }
     }
