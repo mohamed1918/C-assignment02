d48b47e3e75f4ebd05d9f2e8ee2aac344803f438
mohamed1918 <memoohossam2004@gmail.com>
2025-06-25 14:48:52 +0300

    Q3

--- task2/Program.cs
+++ task2/Program.cs
@@ -13,11 +13,16 @@ namespace task2
             #endregion
 
             #region Q2
-            string str = "123abc";
-            int val = int.Parse(str);
-            Console.WriteLine(val);
+            //string str = "123abc";
+            //int val = int.Parse(str);
+            //Console.WriteLine(val);
             #endregion
 
-        }    
+            #region Q3
+            float num1 = 5.5f, num2 = 2.2f;
+            float sum = num1 + num2;
+            Console.WriteLine(sum);
+            #endregion
+        }
     }
 }
