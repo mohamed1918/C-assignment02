094c116a5906ba9e377537a2a69a3fd25d154c4a
mohamed1918 <memoohossam2004@gmail.com>
2025-06-25 15:01:01 +0300

    Q4

--- task2/Program.cs
+++ task2/Program.cs
@@ -19,9 +19,15 @@ namespace task2
             #endregion
 
             #region Q3
-            float num1 = 5.5f, num2 = 2.2f;
-            float sum = num1 + num2;
-            Console.WriteLine(sum);
+            //float num1 = 5.5f, num2 = 2.2f;
+            //float sum = num1 + num2;
+            //Console.WriteLine(sum);
+            #endregion
+
+            #region Q4
+            string str = "mohammed";
+            string sub = str.Substring(0,5);
+            Console.WriteLine(sub);
             #endregion
         }
     }
